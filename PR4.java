1fb6c948b67829c923c12821560a72385f1f723f
Darshil Rana <darshilrana158@gmail.com>
2026-04-23 18:37:11 +0530

    Rename Main class to PR4 and adjust formatting

--- PR4.java
+++ PR4.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class PR4 {
     public static void insertElements(int arr[], int num) {
         Random rand = new Random();
         for (int i = 0; i < num; i++) {
