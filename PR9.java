82783968f7b234478898a43045cf44a4c5006d68
Darshil Rana <darshilrana158@gmail.com>
2026-04-23 18:41:10 +0530

    Update and rename PR9.txt to PR9.java

--- PR9.java
+++ PR9.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class CoinChange {
+public class PR9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
